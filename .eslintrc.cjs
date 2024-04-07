8fcf2f8a2a3b3550a784eebd7e2afe5372a80444
Mats Andresen <mats.andresen@gmx.de>
2024-04-07 11:14:43 +0200

    chore: bump @sveltejs/kit to 2.5.5

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,28 +1,34 @@
+/** @type { import("eslint").Linter.Config } */
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-  plugins: ['svelte3', '@typescript-eslint'],
-  ignorePatterns: ['*.cjs', '*.js'],
-  overrides: [
-    {
-      files: ['*.svelte'],
-      processor: 'svelte3/svelte3',
-      rules: {
-        'no-undef': 'off' // recommencted to disable this rule when using typescript
-      }
-    }
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:svelte/recommended',
+    'prettier'
   ],
-  settings: {
-    'svelte3/typescript': () => require('typescript')
-  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   parserOptions: {
     sourceType: 'module',
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
+    extraFileExtensions: ['.svelte']
   },
   env: {
     browser: true,
     es2017: true,
     node: true
-  }
+  },
+  overrides: [
+    {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser'
+      },
+      rules: {
+        'no-undef': 'off' // recommencted to disable this rule when using typescript
+      }
+    }
+  ]
 }
